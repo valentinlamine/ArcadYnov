2eae9fb355f30069db28481560860fc47fcc13e9
M1txY <113921255+M1txY@users.noreply.github.com>
2023-02-08 16:23:19 +0100

    ping

--- arcad/main.cpp
+++ arcad/main.cpp
@@ -1,23 +1,40 @@
 #include <SFML/Graphics.hpp>
-
+//prout mdr 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(1080, 500), "Pong!");
+	sf::RectangleShape rect1(sf::Vector2f(100, 200));
+	sf::RectangleShape rect2(sf::Vector2f(100, 200));
+    
+    
+	rect1.setFillColor(sf::Color::Red);
+	rect2.setFillColor(sf::Color::Blue);
+	rect1.setPosition(0, 0);
+	rect2.setPosition(980, 0);
+    
+	
+
+    
+    
+
+	
 
     while (window.isOpen())
     {
         sf::Event event;
-        while (window.pollEvent(event))
+        while (window.pollEvent(event))  //Boucle while qui dit que tant que l'ecrans et ouvert et que on ne ferme pas le prog continu de tourner    
         {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
-        window.clear();
-        window.draw(shape);
+        
+                            //Dessine les deux rectangle rouge et bleu 
+		window.clear();
+		window.draw(rect1);
+		window.draw(rect2);
         window.display();
+
     }
 
     return 0;
