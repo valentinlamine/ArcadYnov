687ceffc3e9fd1dfb6089fdbf013b0bda55da852
M1txY <113921255+M1txY@users.noreply.github.com>
2023-02-08 17:35:09 +0100

    tr

--- arcad/main.cpp
+++ arcad/main.cpp
@@ -1,16 +1,37 @@
 #include <SFML/Graphics.hpp>
-//prout mdr 
+
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1080, 500), "Pong!");
-	sf::RectangleShape rect1(sf::Vector2f(100, 200));
-	sf::RectangleShape rect2(sf::Vector2f(100, 200));
+	sf::CircleShape shape(20.f);
+	sf::RectangleShape rect1(sf::Vector2f(50, 200));
+	sf::RectangleShape rect2(sf::Vector2f(50, 200));
+ 
     
     
 	rect1.setFillColor(sf::Color::Red);
 	rect2.setFillColor(sf::Color::Blue);
 	rect1.setPosition(0, 0);
-	rect2.setPosition(980, 0);
+	rect2.setPosition(1035, 0);
+	shape.setFillColor(sf::Color::Green);
+	shape.move(500, 250);
+	
+	// move up rect2 par rapport a la touche z
+	bind z = sf::Keyboard::Z;
+	// move down rect2 par rapport a la touche s
+	bind s = sf::Keyboard::S;
+	if (sf::Keyboard::isKeyPressed(z))
+		rect1.move(0, -1);
+		shape.move(0, -1);
+	
+	if (sf::Keyboard::isKeyPressed(s))
+		rect1.move(0, 1);
+		shape.move(0, 1);
+	
+	
+
+    
+
     
 	
 
@@ -33,6 +54,7 @@ int main()
 		window.clear();
 		window.draw(rect1);
 		window.draw(rect2);
+		window.draw(shape);
         window.display();
 
     }
