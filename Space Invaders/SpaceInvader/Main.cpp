73a2e655a032d0c8a7ad27d1c686b6cace98b4c3
JOEL <joelhappy16@gmail.com>
2023-03-01 09:16:49 +0100

    updt

--- Space Invaders/SpaceInvader/Main.cpp
+++ Space Invaders/SpaceInvader/Main.cpp
@@ -11,6 +11,27 @@ int main()
 
     //boucle pour afficher la fenêtre 
     while (window.isOpen()) {
+<<<<<<< Updated upstream
+=======
+        Event event;
+        while (window.pollEvent(event)) {
+            //gestion des inputs 
+            InputHandler(event, window);
+          
+        }
+        //couleur de la fenêtre
+        window.clear(Color::Black);
+
+        //les dessin sur l'écran
+        //window.draw();
+
+        //dessiner à l'écran
+        window.display();
+
+
+       
+    }
+>>>>>>> Stashed changes
 
     }
 
